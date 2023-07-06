2c06edd954c14420a11b74ea512cef4c944a7c0c
Abhay Goyal <abhayg47@gmail.com>
2023-07-06 16:36:03 -0400

    shapley value calculation

--- functions.py
+++ functions.py
@@ -7,7 +7,10 @@ import matplotlib.pyplot as plt
 from pyvis.network import Network
 
 def draw(G, pos, measures, measure_name):
-    sizes = np.fromiter(measures.values(), float)
+    if type(measures) is list:
+        sizes = np.fromiter(measures, float)
+    else:
+        sizes = np.fromiter(measures.values(), float)
     colormap = mpl.cm.viridis
     normalize = mpl.colors.Normalize(vmin=sizes.min(), vmax=sizes.max())
 
@@ -28,7 +31,6 @@ def saveGraph(G, pos, measures, measure_name):
     for n in G.nodes(data=True):
         n[1]['title']= "%.3f" % sizes[counter]
         n[1]['color'] = colormap(sizes[counter]/sizes.max())
-        print(n[1]['color'])
         counter += 1
 
     #Visualize the network
@@ -36,3 +38,23 @@ def saveGraph(G, pos, measures, measure_name):
     net.from_nx(G)
     # net.save_graph("output/model-network-"+measure_name+".html")
     net.show("output/model-network-"+measure_name+".html")
+
+def shapleyGame1(G: nx.Graph) -> list[float]:
+    sv = []
+    for i in range(1, len(G.nodes)+1):
+        sv.append(1/(1+G.degree[i]))
+        for j in range(0, len(G.nodes)):
+            if i != j:
+                if G.has_edge(i,j):
+                    sv[i-1] += 1/(1+G.degree[j])
+    return sv
+
+def shapleyGame2(G: nx.Graph, k: int) -> list[float]:
+    sv = []
+    for i in range(1, len(G.nodes)+1):
+        sv.append(min(1,k/(1+G.degree[i])))
+        for j in range(0, len(G.nodes)):
+            if i != j:
+                if G.has_edge(i,j):
+                    sv[i-1] += max(0,(G.degree[j]-k+1)/(G.degree[j]*(1+G.degree[j])))
+    return sv
